3d310feb445445ca9908f2b271afea9d28df8f00
Brian Bai <baitongqiang@gmail.com>
2016-03-23 17:33:11 +0800

    config writer

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-PATH=/home/brian/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin
+PATH=/home/brian/anaconda3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 cd /home/brian/github/owanotify
 ./mailchecker.py
