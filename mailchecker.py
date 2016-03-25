8f2b9e0501f45546b2ad9db502022e6893fe3135
Brian Bai <baitongqiang@gmail.com>
2016-03-25 11:14:55 +0800

    change log name

--- mailchecker.py
+++ mailchecker.py
@@ -55,8 +55,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO,
           format='%(asctime)s %(filename)s[line:%(lineno)d]%(levelname)s %(message)s',
           datefmt='%a, %d %b %Y %H:%M:%S',
-          filename='mailchecker.log',
-          filemode='w')
+          filename='owamailchecker.log',
+          filemode='w+')
             
     mailChecker = MailChecker()
     mailChecker.getconfig()
